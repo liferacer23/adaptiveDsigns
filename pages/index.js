d3df540b2cb9f2edbf1639c5530dd98f5f0d1985
Binyam Getachew <binyamg23@gmail.com>
2022-12-10 13:38:28 +0300

    added language propery

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head, { Html } from "next/head";
 
 export default function Home() {
   return (
     <>
-
+      <Html lang="en">
+        <Head>
+          <title>Adaptive Designs</title>
+          <meta name="description" content="Adaptive Designs" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+      </Html>
       <h1 className="text-3xl font-bold underline">Hi</h1>
     </>
-  )
+  );
 }
