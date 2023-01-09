e7e71e989c5cf999e3de1bdb5728e0509d2d4a8c
Binyam Getachew <binyamg23@gmail.com>
2023-01-09 11:46:57 +0300

    minor naming fix

--- components/InteriorModal.js
+++ components/InteriorModal.js
@@ -20,11 +20,11 @@ export default function InteriorModal() {
     "/Portfolio/Interior/Commercial/8/1.jpg",
   ];
 
-  const hospitality = [
-    "/Portfolio/Interior/Hospitality/1/2.jpeg",
-    "/Portfolio/Interior/Hospitality/2/1.jpg",
-    "/Portfolio/Interior/Hospitality/3/1.jpg",
-    "/Portfolio/Interior/Hospitality/4/1.jpg",
+  const residential = [
+    "/Portfolio/Interior/Residential/1/2.jpeg",
+    "/Portfolio/Interior/Residential/2/1.jpg",
+    "/Portfolio/Interior/Residential/3/1.jpg",
+    "/Portfolio/Interior/Residential/4/1.jpg",
   ];
 
   return (
@@ -87,7 +87,7 @@ export default function InteriorModal() {
             </div>
           </div>
         </div>
-        <AutoSlider images={hospitality} />
+        <AutoSlider images={residential} />
       </>
       <>
         <div className="bg-secondary flex lg:flex-row sm:flex-col xs:flex-col lg:items-start xs:justify-start sm:justify-start lg:justify-between text-white pt-14 h-[fit-content] pb-4 lg:px-10">
@@ -118,6 +118,36 @@ export default function InteriorModal() {
           </div>
         </div>
         <AutoSlider images={commercial} />
+      </>
+      <>
+        <div className="bg-secondary flex lg:flex-row sm:flex-col xs:flex-col lg:items-start xs:justify-start sm:justify-start lg:justify-between text-white pt-14 h-[fit-content] pb-4 lg:px-10">
+          <div className="flex gap-5 items-start justify-center xs:w-full  sm:w-full lg:w-[40%] ">
+            <div className="flex items-center justify-center p-4">
+              <span className="xl:text-[3rem] xs:text-[1.7rem] sm:text-[1.7rem]">
+                RESIDENTIAL INTERIORS
+              </span>
+            </div>
+          </div>
+          <div className=" flex gap-5 items-start justify-center xs:w-full  sm:w-full lg:w-[40%]  mt-5 p-2">
+            <Image
+              src={quots}
+              alt="quots"
+              width={25}
+              height={25}
+              className="mt-4"
+            />
+
+            <div className="h-full flex item-center justify-center lg:mt-5 lg:mr-5">
+              <span className="xs:text-[0.7rem] sm:text-[0.7rem] lg:text-[0.8rem]">
+                Is your office tailored to the specific needs of your business
+                and does it help express your company vision? Is your office
+                tailored to the specific needs of your business and does it help
+                express your company vision?
+              </span>
+            </div>
+          </div>
+        </div>
+        <AutoSlider images={residential} />
       </>
     </>
   );
