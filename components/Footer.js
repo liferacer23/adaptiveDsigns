32739cf9020780abffefe94f0142ef1a8e39202f
Binyam Getachew <binyamg23@gmail.com>
2023-01-21 18:56:16 +0300

    accessibilty improvements

--- components/Footer.js
+++ components/Footer.js
@@ -10,27 +10,41 @@ export default function Footer() {
   return (
     <div className="flex flex-col items-center justify-center py-10 h-full bg-secondary w-full mt-20 gap-3 relative">
       <div className="flex items-center justify-center bg-white shadow-xl p-2 rounded-full w-20 h-20 absolute top-[-30px] ">
-        <Image src={logo} className="w-20 h-20"  alt="logo" />
+        <Image src={logo} className="w-20 h-20" alt="logo" />
       </div>
       <span className="text-white text-sm mt-5">adaptivedesigns.com</span>
       <div className="flex gap-2 items-center justify-center">
-        <Link href="https://www.instagram.com/adaptivedesigns" 
-         target="_blank"
+        <Link
+          name="instagram"
+          href="https://www.instagram.com/adaptivedesigns"
+          target="_blank"
         >
           {" "}
           <AiOutlineInstagram
             style={{ fontSize: "1rem", color: "#fff" }}
           />{" "}
         </Link>
-        <Link href="https://www.linkedin.com/company/adaptivedesigns" target="_blank">
+        <Link
+          name="linkedin"
+          href="https://www.linkedin.com/company/adaptivedesigns"
+          target="_blank"
+        >
           {" "}
           <FaLinkedinIn style={{ fontSize: "1rem", color: "#fff" }} />{" "}
         </Link>
-        <Link href="https://www.facebook.com/adaptivedesigns" target="_blank">
+        <Link
+          name="facebook"
+          href="https://www.facebook.com/adaptivedesigns"
+          target="_blank"
+        >
           {" "}
           <GrFacebookOption style={{ fontSize: "1rem", color: "#fff" }} />{" "}
         </Link>
-        <Link href="https://t.me/adaptivedesigns" target="_blank">
+        <Link
+          name="telegram"
+          href="https://t.me/adaptivedesigns"
+          target="_blank"
+        >
           {" "}
           <FaTelegramPlane style={{ fontSize: "1rem", color: "#fff" }} />{" "}
         </Link>
